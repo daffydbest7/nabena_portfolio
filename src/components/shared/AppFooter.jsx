b35d3b1b680503dae50b7d1e070ef951bbbac5e2
daffybest7 <daffydbest@gmail.com>
2023-12-15 21:56:17 +0100

    new changes

--- src/components/shared/AppFooter.jsx
+++ src/components/shared/AppFooter.jsx
@@ -1,9 +1,5 @@
 import {
-	FiGithub,
-	FiTwitter,
 	FiLinkedin,
-	FiGlobe,
-	FiYoutube,
 	FiFacebook,
 	FiInstagram
 } from 'react-icons/fi';
@@ -33,8 +29,8 @@ const AppFooter = () => {
 			<div className="pt-20 sm:pt-30 pb-8 mt-20 border-t-2 border-primary-light dark:border-secondary-dark">
 				{/* Footer social links */}
 				<div className="font-general-regular flex flex-col justify-center items-center mb-12 sm:mb-28">
-					<p className="text-3xl sm:text-4xl text-primary-dark dark:text-primary-light mb-5">
-						Follow me
+					<p className="text-2xl sm:text-3xl text-primary-dark dark:text-primary-light mb-5">
+						Let's Connect
 					</p>
 					<ul className="flex gap-4 sm:gap-8">
 						{socialLinks.map((link) => (
