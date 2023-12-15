18cd295b5669e55535a2000f17c1f1b9211f1d31
daffybest7 <daffydbest@gmail.com>
2023-12-15 22:34:43 +0100

    changes

--- src/components/shared/AppFooterCopyright.jsx
+++ src/components/shared/AppFooterCopyright.jsx
@@ -1,14 +1,14 @@
 function AppFooterCopyright() {
 	return (
 		<div className="font-general-regular flex justify-center items-center text-center">
-			<div className="text-lg text-ternary-dark dark:text-ternary-light">
+			<div className="text-sm md:text-xl lg:text-xl text-ternary-dark dark:text-ternary-light">
 				&copy; {new Date().getFullYear()}
 				
 					Developed by
 				<a
 					href="https://davidlawrence.onrender.com"
 					target="__blank"
-					className=" dark:text-secondary-light font-medium  hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500 text-green-500"
+					className=" dark:text-secondary-light font-medium  hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500 text-green-500 "
 				>
 					David Lawrence
 				</a>
