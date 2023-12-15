281504d4332e648e5b823b86cbf1b7af2d807c1d
daffybest7 <daffydbest@gmail.com>
2023-12-15 21:30:19 +0100

    init

---
+++ src/data/aboutMeData.js
@@ -0,0 +1,14 @@
+export const aboutMeData = [
+	{
+		id: 1,
+		bio: `As a seasoned professional in ICT consultancy, my extensive experience positions me as a catalyst for technological innovation and operational excellence. Specializing in database and network administration, I adeptly integrate cutting-edge solutions to optimize data management and enhance network performance. 
+		Concurrently, my proficiency as a web developer ensures the creation of visually captivating and functionally robust websites, with a keen eye on industry trends Recognized for my adept problem-solving skills and adaptability, I consistently surpass client expectations. In collaborative environments, I effectively 
+		communicate intricate technical concepts to diverse stakeholders, fostering successful project outcomes. What sets me apart is not only my commitment to continuous learning in the dynamic tech landscape but also my proficiency in data analysis. This analytical expertise enhances decision-making processes, contributing 
+		to business intelligence and strategic planning. In this multifaceted role, I am poised to leverage my comprehensive skill set, spanning ICT consultancy, database administration, web development, and data analysis. My commitment to staying ahead of technological advancements positions me as a valuable asset, ready to 
+		tackle the complexities of the digital age and drive organizational success.`,
+	},
+	{
+		id: 2,
+		bio: 'CEO, eM & Haitch ICT HUB',
+	},
+];
---
+++ src/data/projects.js
@@ -0,0 +1,57 @@
+// Import images
+import WebImage1 from '../images/web-project-1.jpg';
+import WebImage2 from '../images/web-project-2.jpg';
+import MobileImage1 from '../images/mobile-project-1.jpg';
+import MobileImage2 from '../images/mobile-project-2.jpg';
+import UIImage1 from '../images/ui-project-1.jpg';
+import UIImage2 from '../images/ui-project-2.jpg';
+
+export const projectsData = [
+	{
+		id: 1,
+		title: 'Diabetes Prediction Analysis',
+		category: 'Data Analysis',
+		url: "https://davidlawrence.onrender.com",
+		img: WebImage2,
+		ProjectHeader: {
+			title: 'Project Management UI - From Context',
+			publishDate: 'Jul 26, 2021',
+			tags: 'UI / Frontend',
+		},
+	},
+	{
+		id: 2,
+		title: 'Student School Performance',
+		category: 'Data Analysis',
+		url: "https://davidlawrence.onrender.com",
+		img: MobileImage2,
+	},
+	{
+		id: 3,
+		title: 'Network Facility Installation',
+		category: 'Project Management',
+		url: "https://davidlawrence.onrender.com",
+		img: UIImage1,
+	},
+	{
+		id: 4,
+		title: 'Analysis Using R Programming',
+		category: 'Data Analysis',
+		url: "https://davidlawrence.onrender.com",
+		img: UIImage2,
+	},
+	{
+		id: 5,
+		title: 'React Social App',
+		category: 'Web Development',
+		url: "https://davidlawrence.onrender.com",
+		img: MobileImage1,
+	},
+	{
+		id: 6,
+		title: 'Database setup and Monitoring',
+		category: 'Database Administration',
+		url: "https://davidlawrence.onrender.com",
+		img: WebImage1,
+	},
+];
